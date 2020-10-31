4ad09308c8d31a9a78b769753dbb9ddae61e7591
Jose Luis Alvarez <joseluis_fps@hotmail.com>
2020-10-30 19:45:38 -0600

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    // const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     const categoryToAdd = 'Pokemon';
+    //     setCategories([categoryToAdd, ...categories]);
+    // }
+
+    return(
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+
+            <hr/>
+
+            <ol>
+                {
+                    categories.map(category =>  
+                        <GifGrid 
+                            key={category} 
+                            category={category} />)
+                }
+            </ol>
+        </>
+    )
+}
+
+export default GifExpertApp;
